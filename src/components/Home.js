2ef6a6b36f3650b86de651583cf617c79f06952a
Roland Shen <rolandtshen@gmail.com>
2019-07-28 16:25:15 -0700

    Add illustrations, three column layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import man from '../assets/man.png';
+import woman from '../assets/woman.png';
 
 class Home extends Component {
   render() {
     return (
-      <div className="container double-center">
-        <a href="localhost:3000" className="logo">clubhouse</a>
-        <h1 className="primary-title">Meet someone, monthly</h1>
-        <h3 className="secondary-title">clubhouse is the community for ideators, makers, and creatives.</h3>
-        <a href="/apply" className="apply">Apply</a>
+      <div className="hero">
+        <div className="hero-illustration" id="right">
+          <img src={woman} />
+        </div>
+        <div className="hero-illustration" id="left">
+          <img src={man} />
+        </div>
+        <div className="hero-wrapper">
+          <a href="#" className="logo">clubhouse</a>
+          <h1 className="hero-title">Meet someone, every month</h1>
+          <h3 className="hero-subtitle">clubhouse is the community for ideators, makers, and creatives.</h3>
+          <a href="/apply" className="apply">Apply</a>
+        </div>
       </div>
     );
   }
