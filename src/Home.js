865301cc2b75d4023b35dfd342042ddf2d9f50c0
Roland Shen <rolandtshen@gmail.com>
2019-07-28 00:17:19 -0700

    Frontpage v1

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Meet someone, monthly</h1>
-        <h3>clubhouse is the community for ideators, makers, and creatives.</h3>
-        <button>Apply</button>
+      <div className="container double-center">
+        <a href="localhost:3000" className="logo">clubhouse</a>
+        <h1 className="primary-title">Meet someone, monthly</h1>
+        <h3 className="secondary-title">clubhouse is the community for ideators, makers, and creatives.</h3>
+        <a href="https://google.com" className="apply">Apply</a>
       </div>
     );
   }
