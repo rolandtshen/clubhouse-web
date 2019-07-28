5025d5bf2c86bc2cd01b3b8982e0867ac2426e9d
Roland Shen <rolandtshen@gmail.com>
2019-07-27 19:09:03 -0700

    Add bootstrap

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Meet someone, monthly</h1>
+        <h3>clubhouse is the community for ideators, makers, and creatives.</h3>
+        <button>Apply</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
